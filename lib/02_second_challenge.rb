c47525a1056e81964d78c1daa62a775419856d04
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-21 21:26:01 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -11,8 +11,8 @@ def second_challenge
       #your key/value pairs here!
       :patriarch => {},
        :matriarch => {},
-       :hero => {},
-       :herroine_friends => []
+       :herooine => {},
+       :heroine_friends => []
       }
   }
 
